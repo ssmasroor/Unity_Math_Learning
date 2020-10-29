bf61d59e619d9faf455e31947ae9d727a80f005e
smas <sahab.masroor@gmail.com>
2020-10-29 11:29:01 +0330

    basic vector rotation and interpolation

---
+++ Assets/FromToRotation.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FromToRotation : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        // Sets the rotation so that the transform's y-axis goes along the z-axis
+        transform.rotation = Quaternion.FromToRotation(Vector3.up, transform.forward);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/RotateToward.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateToward : MonoBehaviour
+{
+    public Transform target;
+
+    // Angular speed in degrees per sec.
+    public float speed;
+
+    void Update()
+    {
+        // The step size is equal to speed times frame time.
+        var step = speed * Time.deltaTime;
+
+        // Rotate our transform a step closer to the target's.
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, target.rotation, step);
+    }
+}
